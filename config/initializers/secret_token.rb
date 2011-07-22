f581e98899bb61a92b648669c252c4558c8595e5
Jesus Jackson <jesusejackson@gmail.com>
2011-07-21 22:05:23 -0400

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog4fun::Application.config.secret_token = '05bf61ea9e23c998d4390264a3d93e075f2d7a3ddaac037b3ddfb245ff166a6bd9cf4d204fb79aaeb57e2d78d529278d13b78ae94716b26889edc874c254dbfd'
